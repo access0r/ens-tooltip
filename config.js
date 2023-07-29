7acdeb51f59e80d6d2c2b63a82888b5b12756930
accessor.eth <38918838+access0r@users.noreply.github.com>
2023-07-29 12:27:32 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,27 @@
+{
+    "tooltip": {
+        "position": "absolute",
+        "backgroundColor": "#FFFFFF",
+        "border": "1px solid #000000",
+        "borderRadius": "5px",
+        "padding": "10px",
+        "width": "18%",
+        "overflow": "auto",
+        "display": "flex",
+        "flexWrap": "wrap"
+    },
+    "nameRecord": {
+        "paddingRight": "20%",
+        "borderBottom": "2px solid #eee",
+        "width": "100%"
+    },
+    "dataTitle": {
+        "fontWeight": "bold",
+        "fontSize": "14px",
+        "color": "cornflowerblue"
+    },
+    "textRecord": {
+        "fontSize": "12px",
+        "display": "flex"
+    }
+}
